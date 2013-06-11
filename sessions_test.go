f84abeda17de0b4fcd72d277412f3d3192f206f2
clueless <clueless@thunked.org>
2013-06-11 14:35:17 +0200

    Add test case for race conditions

--- sessions_test.go
+++ sessions_test.go
@@ -7,8 +7,10 @@ package sessions
 import (
 	"bytes"
 	"encoding/gob"
+	"fmt"
 	"net/http"
 	"testing"
+	"time"
 )
 
 // ----------------------------------------------------------------------------
@@ -109,7 +111,7 @@ func TestFlashes(t *testing.T) {
 	hdr = rsp.Header()
 	cookies, ok = hdr["Set-Cookie"]
 	if !ok || len(cookies) != 1 {
-		t.Fatalf("No cookies. Header:", hdr)
+		t.Fatalf("No cookies. Header: % v", hdr)
 	}
 
 	// Round 2 ----------------------------------------------------------------
@@ -168,7 +170,7 @@ func TestFlashes(t *testing.T) {
 	hdr = rsp.Header()
 	cookies, ok = hdr["Set-Cookie"]
 	if !ok || len(cookies) != 1 {
-		t.Fatalf("No cookies. Header:", hdr)
+		t.Fatalf("No cookies. Header: % v", hdr)
 	}
 
 	// Round 4 ----------------------------------------------------------------
@@ -189,6 +191,113 @@ func TestFlashes(t *testing.T) {
 	custom := flashes[0].(FlashMessage)
 	if custom.Type != 42 || custom.Message != "foo" {
 		t.Errorf("Expected %#v, got %#v", FlashMessage{42, "foo"}, custom)
+	}
+}
+
+func RaceRoutine(store *FilesystemStore, req *http.Request, out chan error) {
+	rsp := NewRecorder()
+	// Get a session.
+	session, err := store.Get(req, "session-key")
+	if err != nil {
+		out <- fmt.Errorf("Error getting session: %v", err)
+		return
+	}
+	// Decrement the value of count
+	if rawcount, ok := session.Values["count"]; !ok {
+		out <- fmt.Errorf("Expected \"count\" to exist as a value")
+		return
+	} else {
+		if count, ok := rawcount.(int); !ok {
+			out <- fmt.Errorf("Expected \"count\" to be an integer")
+			return
+		} else {
+			session.Values["count"] = count - 1
+		}
+	}
+	// wait a half a second before saving to induce a race condition with a
+	// predictable outcome
+	time.Sleep(500 * time.Millisecond)
+	// Save.
+	if err = Save(req, rsp); err != nil {
+		out <- fmt.Errorf("Error saving session: %v", err)
+		return
+	}
+	hdr := rsp.Header()
+	cookies, ok := hdr["Set-Cookie"]
+	if !ok || len(cookies) != 1 {
+		out <- fmt.Errorf("No cookies. Header: % v", hdr)
+		return
+	}
+	out <- nil
+}
+
+func TestRace(t *testing.T) {
+	var req *http.Request
+	var rsp *ResponseRecorder
+	var hdr http.Header
+	var err error
+	var ok bool
+	var cookies []string
+	var session *Session
+
+	store := NewFilesystemStore("", []byte("secret-key"))
+
+	// Round 1 ----------------------------------------------------------------
+
+	req, _ = http.NewRequest("GET", "http://localhost:8080/", nil)
+	rsp = NewRecorder()
+	// Get a session.
+	if session, err = store.Get(req, "session-key"); err != nil {
+		t.Fatalf("Error getting session: %v", err)
+	}
+
+	// Initialize a counter to 44
+	session.Values["count"] = 44
+
+	// Save.
+	if err = Save(req, rsp); err != nil {
+		t.Fatalf("Error saving session: %v", err)
+	}
+	hdr = rsp.Header()
+	cookies, ok = hdr["Set-Cookie"]
+	if !ok || len(cookies) != 1 {
+		t.Fatalf("No cookies. Header: % v", hdr)
+	}
+
+	// Round 2 & 3 ------------------------------------------------------------
+	reply := make(chan error)
+	req1, _ := http.NewRequest("GET", "http://localhost:8080/", nil)
+	req2, _ := http.NewRequest("GET", "http://localhost:8080/", nil)
+	req1.Header.Add("Cookie", cookies[0])
+	req2.Header.Add("Cookie", cookies[0])
+
+	go RaceRoutine(store, req1, reply)
+	go RaceRoutine(store, req2, reply)
+
+	err = <-reply
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	err = <-reply
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	// Round 4 ----------------------------------------------------------------
+	req, _ = http.NewRequest("GET", "http://localhost:8080/", nil)
+	req.Header.Add("Cookie", cookies[0])
+	rsp = NewRecorder()
+	// Get a session.
+	if session, err = store.Get(req, "session-key"); err != nil {
+		t.Fatalf("Error getting session: %v", err)
+	}
+
+	if rawcount, ok := session.Values["count"]; !ok {
+		t.Fatalf("Expected \"count\" to exist as a value")
+	} else {
+		if count, ok := rawcount.(int); !ok || count != 42 {
+			t.Fatalf("Expected \"count\" to be an integer of value 42; got %v", count)
+		}
 	}
 }
 
